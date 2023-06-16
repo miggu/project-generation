d2d15b8d7f61f24035ba0dcef87e17ac2f4fc2b3
Miguel <himiggu@gmail.com>
2023-06-16 12:45:29 -0300

    imierda

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, this is impossible. But with Mayte contreras it's easy
+          Learn React, this is impossible
         </a>
       </header>
     </div>
